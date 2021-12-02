0bd1ce4e1ef11d1191589f9a465147cde99bbd19
David Calix <calixd7@gmail.com>
2021-12-02 18:16:33 -0500

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Layout from './hocs/Layout'
+import Home from './components/Home';
+import Blog from './components/Blog';
+import BlogDetail from './components/BlogDetail';
+import Category from './components/Category';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+    <Router>
+        <Layout>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/blog" component={Blog} />
+                <Route exact path="/blog/:id" component={BlogDetail} />
+                <Route exact path="/category/:id" component={Category} />
+            </Switch>
+        </Layout>
+    </Router>               
+);
+
 
 export default App;
---
+++ frontend/src/hocs/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+
+const Layout = (props) => (
+    <div>
+        <Navbar />
+        {props.children}
+    </div>
+));
+
+export default Layout;
